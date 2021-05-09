c00a598ecf32070258952e55a166675b4630370c
angusmacdonald <n10448888@qut.edu.au>
2021-05-09 23:49:09 +1000

    Tool and MessageImplementation

---
+++ MemberToolLibrary/PV.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace MemberToolLibrary
+{
+    public static class PV
+    {
+        public const string header = "   Welcome to the Library Tool Rental System  ";
+        public const string spacer = "###############################################";
+        private const string staffUsername = "staff";
+        private const string staffPassword = "today123";
+
+        public static string Staff()
+        {
+            return staffUsername;
+        }
+
+        public static string Pass()
+        {
+            return staffPassword;
+        }
+
+        public const string categories = @"
+1. Gardening Tools
+2. Flooring Tools
+3. Fencing Tools
+4. Measuring Tools
+5. Cleaning Tools
+6. Painting Tools
+7. Electronic Tools
+8. Electricity Tools
+9. Automotive Tools";
+
+        public const string staffOptions = @"
+1. Add a new tool
+2. Add new pieces of an existing tool
+3. Remove some pieces of a tool
+4. Register a new member
+5. Remove a member
+6. Find the contact number of a member
+0. Return to Main Menu";
+
+
+        public const string memberOptions = @"
+1.Display all the tools of a tool type
+2. Borrow a tool
+3. Return a tool
+4. List all the tools that I am renting
+5. Display the top three(3) most frequently rented tools
+0. Return to Main Menu";
+
+
+        public const string gardeningTools = @"
+1. Line Trimmers
+2. Lawn Mowers
+3. Hand Tools
+4. Wheelbarrows
+5. Garden Power Tools";
+    }
+
+}
---
+++ MemberToolLibrary/StaffOptions/Options.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace MemberToolLibrary.StaffOptions
+{
+    public class Options
+    {
+        public void AddTool()
+        {
+            Console.Clear();
+            Console.SetWindowSize(47, 20);
+            Console.WriteLine("");
+            Console.WriteLine(PV.categories);
+            Console.WriteLine("");
+            Console.WriteLine("Select a category of tool 1-9, or 0 to return to previous menu");
+
+        }
+    }
+}
